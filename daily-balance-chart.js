2e4d7178639d20bc1a745f30b52c4f4fea362912
Marko Knöbl <marko.kn@gmail.com>
2016-09-15 13:49:41 +0200

    daily balance chart: make if faster by displaying fewer points

--- daily-balance-chart.js
+++ daily-balance-chart.js
@@ -4,12 +4,30 @@ var inverseBalances = function(balanceList) {
   });
 };
 
+var filterPoints = function(points) {
+  if (points.length < 150) {
+    return points;
+  }
+  var newPoints = points.filter(function(element, index, array) {
+    if (index === 0 || index === points.length-1) {
+      return true;
+    }
+    if (Math.abs(points[index].balance - points[index+1].balance) > 50 ||
+        Math.abs(points[index-1].balance - points[index].balance) > 50) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return newPoints;
+};
+
 var dailyBalanceChart = function() {
   return React.createElement(NVD3Chart, {
     type: 'lineChart',
     datum: [{
       key: 'Total assets',
-      values: inverseBalances(dataset.getDailyBalances('world'))
+      values: filterPoints(inverseBalances(dataset.getDailyBalances('world')))
     }],
     x: function(d) {return new Date(d.date)},
     y: 'balance',
